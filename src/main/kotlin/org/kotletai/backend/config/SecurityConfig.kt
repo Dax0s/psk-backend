e7bd0b018c8dd22ed382cfd180e4dcf88fd75321
Dax0s <dev@dklimavicius.com>
2026-05-20 22:38:22 +0300

    Uptade cors

--- src/main/kotlin/org/kotletai/backend/config/SecurityConfig.kt
+++ src/main/kotlin/org/kotletai/backend/config/SecurityConfig.kt
@@ -51,9 +51,14 @@ class SecurityConfig {
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:3000")
+        configuration.allowedOrigins =
+            listOf(
+                "http://localhost:3000",
+                "https://psk.dklimavicius.com",
+            )
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
         configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = true
 
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
